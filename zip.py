d2e252764b0a2b501ee7f2c527bb4dbfc2cabdaa
yejin <gut2892@gmail.com>
2024-04-09 18:51:55 +0900

    hw

---
+++ zip.py
@@ -0,0 +1,5 @@
+def hero():
+    names = ['Peter Parker', 'Clark Kent', 'Wade Wilson', 'Bruce Wayne']
+    heroes = ['Spiderman', 'Superman', 'Deadpool', 'Batman']
+    for name, hero in zip(names, heroes):
+        print(f'{name} is actually {hero}')
